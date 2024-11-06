e9c9d9b412d7420e1bc87f5a05bff6333a545003
Nicolas Droux <nicolas.droux.2b@gmail.com>
2024-11-06 16:55:11 +0100

    Ref #00003 - Create some API routes, only createAirlineCompany route works atm

---
+++ src/app/api/(CREATE)/createAirlineCompany/route.ts
@@ -0,0 +1,36 @@
+import {NextRequest, NextResponse} from "next/server";
+import prisma from "../../../../../lib/prisma";
+
+export async function POST(req: NextRequest) {
+    if (!process.env.DATABASE_URL) {
+        return NextResponse.json(
+            { error: "DATABASE_URL is not set" },
+            { status: 500 }
+        );
+    }
+    try {
+        const body = await req.json();
+        const { companyName } = body;
+
+        if (!companyName) {
+            return NextResponse.json(
+                { error: "Missing required fields" },
+                { status: 400 }
+            );
+        }
+
+        const airlineCompany = await prisma.airlineCompany.create({
+            data: {
+                companyName,
+            },
+        });
+
+        return NextResponse.json(airlineCompany, { status: 201 });
+    } catch (error) {
+        console.error("Error during ailine company creation:", error);
+        return NextResponse.json(
+            { error: "Airline company creation failed" },
+            { status: 400 }
+        );
+    }
+}
--- src/app/api/test/route.ts
+++
@@ -1,5 +0,0 @@
-import {NextResponse} from "next/server";
-
-export const GET = async () => {
-    return new NextResponse("Hello, ceci est un test pour voir si le système de route fonctionne.")
-}
