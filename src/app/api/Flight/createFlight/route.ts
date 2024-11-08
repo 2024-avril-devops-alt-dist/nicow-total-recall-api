0e70b5b600d11f48cf54f93debbf752607e70b83
Nicolas Droux <nicolas.droux.2b@gmail.com>
2024-11-08 14:55:16 +0100

    Ref #00003 - Rework architecture and add basic required routes for auth, see documentation of auth.js

---
+++ src/app/api/Flight/createFlight/route.ts
@@ -0,0 +1,52 @@
+import {NextRequest, NextResponse} from "next/server";
+import prisma from "../../../../../lib/prisma";
+
+export async function POST(req: NextRequest) {
+    if (!process.env.DATABASE_URL) {
+        return NextResponse.json(
+            { error: "DATABASE_URL is not set" },
+            { status: 500 }
+        );
+    }
+    try {
+        const body = await req.json();
+        const { bookingOpenStatus,
+            departureAirport,
+            arrivalAirport,
+            departureDate,
+            arrivalDate,
+            airlineCompanyId,
+        } = body;
+
+        if (!bookingOpenStatus
+            || !departureAirport
+            || !arrivalAirport
+            || !departureDate
+            || !arrivalDate
+            || !airlineCompanyId) {
+            return NextResponse.json(
+                { error: "Missing required fields" },
+                { status: 400 }
+            );
+        }
+
+        const flight = await prisma.flight.create({
+            data: {
+                bookingOpenStatus,
+                departureAirport,
+                arrivalAirport,
+                departureDate,
+                arrivalDate,
+                airlineCompanyId,
+            },
+        });
+
+        return NextResponse.json(flight, { status: 201 });
+    } catch (error) {
+        console.error("Error during flight creation:", error);
+        return NextResponse.json(
+            { error: "Location creation failed" },
+            { status: 400 }
+        );
+    }
+}
