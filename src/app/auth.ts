b803bb3451e6d9bda28b8d8bfa1284907e4e19bd
Nicolas Droux <nicolas.droux.2b@gmail.com>
2024-12-03 17:19:20 +0100

    Ref #00005 - Rename route for auth

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import Google from "@auth/core/providers/google";
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [],
+    providers: [Google],
 })
