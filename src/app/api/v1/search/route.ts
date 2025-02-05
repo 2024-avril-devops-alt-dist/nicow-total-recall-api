be63393aaf9c34b745eee19a31512c4c8fc1b641
Nicolas Droux <nicolas.droux@ecedi.fr>
2025-02-05 14:29:31 +0100

    Ref #00018 - Add limit (set to 10) to search api route

--- src/app/api/v1/search/route.ts
+++ src/app/api/v1/search/route.ts
@@ -19,6 +19,7 @@ export async function GET(req: NextRequest) {
 
         const flights = await prisma.flight.findMany({
             where,
+            take: 10,
             include: {
                 departureAirport: {
                     include: {
