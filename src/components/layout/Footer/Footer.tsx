0a041394c512056fa54d81419a939bf24abbca04
Nicolas Droux <nicolas.droux@ecedi.fr>
2025-02-05 16:17:30 +0100

    Ref #00018 - Remove footer on homepage

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,8 +1,20 @@
+"use client";
 import styles from "@/components/layout/Footer/Footer.module.css";
+import {usePathname} from "next/navigation";
+
 export function Footer() {
+    const pathname = usePathname();
+    const isHomePage = pathname === "/";
+
     return (
-        <footer className={styles.footer}>
-            <p>Ceci est le footer. Tout droit réservés.</p>
-        </footer>
+        <>
+            {isHomePage ?
+                '' :
+                <footer className={styles.footer}>
+                    <p>Ceci est le footer. Tout droit réservés.</p>
+                </footer>
+            }
+
+        </>
     )
 }
