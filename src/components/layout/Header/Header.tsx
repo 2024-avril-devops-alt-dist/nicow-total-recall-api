b149b3ca70f9a81e293537cfb4545cc695d0cbb4
Nicolas Droux <nicolas.droux@ecedi.fr>
2025-02-07 10:47:41 +0100

    Ref #00018 - Fix header & middleware

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,13 +1,10 @@
 import { Nav } from "@/components/layout/Nav/Nav";
 import styles from "@/components/layout/Header/Header.module.css";
-import {headers} from "next/headers";
 
 export function Header() {
-    const headersList = headers() as Headers;
-    const isHomePage = headersList.get("X-Is-Home") === "true";
 
     return (
-        <header className={isHomePage ? styles.headerHome : styles.header}>
+        <header className={styles.header}>
             <Nav />
         </header>
     );
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,13 @@
-import { auth, signIn } from "@/auth";
+import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
     const session = await auth();
     const isHome = request.nextUrl.pathname === "/";
-    const response = NextResponse.next();
 
     if (session || isHome) {
-        response.headers.set("X-Is-Home", isHome ? "true" : "false");
-        return response;
+        return NextResponse.next();
     }
 
     const redirectUrl = new URL('/api/auth/signin', request.url);
